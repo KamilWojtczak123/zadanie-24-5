2afad5db43e136dfa42d22571a3cae668a755c27
Kamil Wojtczak <kamil.wojtczak1@gmail.com>
2018-05-29 17:36:50 +0200

    Update stylePoints

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,7 +1,18 @@
-const calculateStylePoints = (styleNotes) => {
-  const max = Math.max(styleNotes[0], styleNotes[1], styleNotes[2], styleNotes[3], styleNotes[4]);
-  const min = Math.min(styleNotes[0], styleNotes[1], styleNotes[2], styleNotes[3], styleNotes[4]);
-    return styleNotes[0] + styleNotes[1] + styleNotes[2] + styleNotes[3] + styleNotes[4] - max - min;
+'use strict';
+const calculateStylePoints = styleNotes => {
+  if (Object.prototype.toString.call(styleNotes) !== '[object Array]')
+    throw TypeError('Style notes must be an array.');
+  if (styleNotes.length !== 5)
+    throw Error(`There must be 5 notes, not ${styleNotes.length}!!!`);
+  const result = styleNotes
+    .sort((a, b) => {
+      return a - b;
+    })
+    .slice(1, 4)
+    .reduce((acc, curr) => {
+      return acc + curr;
+    });
+  return result;
 };
 
 module.exports = calculateStylePoints;
